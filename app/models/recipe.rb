9e68c04fac89eaa8618f4c2b22a48f53a7155353
Jviveiros <jviveiros@github.com>
2017-01-04 17:03:20 -0800

    Add card partial for recipe show

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_mnay :directions
+  has_many :directions
   has_many :ingredients
+
+  validates :description, length: {maximum: 140,
+    too_long: "140 characters is the maximum allowed"} 
 end
