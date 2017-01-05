c91d550dc350e54d5a711489dff00ea3511ae574
Andrew Bruckman <abruckman09@gmail.com>
2017-01-05 14:53:31 -0800

    add bio form

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  bindEventHandlers();
+});
+
+var bindEventHandlers = function(){
+  showBioForm();
+}
+
+var showBioForm = function(){
+  $('.edit-btn').on('click', function(e){
+    e.preventDefault();
+    $('.bio-text').hide();
+    $('.bio-form').show();
+  })
+};
