8182c6ec651d5d98cfb96045b436ca9de3536fa7
Andrew Bruckman <abruckman09@gmail.com>
2017-01-06 12:37:09 -0800

    create category page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,15 +6,33 @@ class RecipesController < ApplicationController
   def new
     @category = Category.find(params[:category_id])
     @recipe = @category.recipes.new
-    @recipe.ingredients.new
-    @recipe.directions.new
 
-    p BootstrapForm::FormBuilder.new(:recipe, @recipe, self, {})
+  end
+
+  def create
+    p "*"*50
+    p params[:category_id]
+    p "*"*50
+    @category = Category.find(params[:category_id])
+    p @category
+    @recipe = @category.recipes.new(recipes_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to edit_category_recipe_path(@category, @recipe)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:category_id])
+    @recipe = Recipe.find(params[:id])
   end
 
   private
     def recipes_params
-      params.require(:user).permit(:id, :category_id, :description, :prep_time, :cook_time, :difficult, :image_link, :directions, :ingredients)
+      params.require(:recipe).permit(:id, :category_id, :title, :description, :prep_time, :cook_time, :difficulty, :image_link, :directions, :ingredients)
     end
 
 end
