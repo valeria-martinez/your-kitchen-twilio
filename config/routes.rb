580e15d63848df2cc7c265eccc220f0fa98d1338
Jviveiros <jviveiros@github.com>
2017-01-05 16:10:37 -0800

    Sessions routes and controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
 
-  get 'users/new'
+  get '/sessions/new' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/sessions' => 'sessions#destroy'
 
   root to: 'application#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
